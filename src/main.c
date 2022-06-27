4b132df97ab4bdbe06b5b54ccffa7ab7c2b4033f
v38armageddon <thev38armageddon@outlook.fr>
2022-06-27 15:22:38 +0200

    Update main.c and Makefile

--- src/main.c
+++ src/main.c
@@ -1,22 +1,6 @@
 #include <stdio.h>
 
-#define PROGRESS_BAR_SIZE 40
-
 char user_input[20];
-
-void print_n_chars(int n, int c) {
-    while (n-- > 0) {
-        putchar(c);
-    }
-}
-
-void display_progress_bar(int p) {
-    putchar('\r');
-    putchar('[');
-    print_n_chars(PROGRESS_BAR_SIZE * p / 100, '=');
-    print_n_chars(PROGRESS_BAR_SIZE - PROGRESS_BAR_SIZE * p / 100, '-');
-    putchar(']');
-}
 
 int main(void) {
     printf("Foo History\n\n");
@@ -28,11 +12,6 @@ int main(void) {
         scanf("%s", user_input);
         if (user_input == "y") {
             printf("Let's start the game!\n");
-            int p;
-            for (p = 0; p <= 100; ++p) {
-                display_progress_bar(p);
-                Sleep(100);
-            }
         }
         else if (user_input == "n") {
             printf("Okay, have a good day!\n");
